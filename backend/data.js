7855ff117ad33682fa2b8811efcf648274850382
poojaDaithankar <pujadaithankar@gmail.com>
2021-05-07 15:18:57 +0530

    products in Mongodb

--- backend/data.js
+++ backend/data.js
@@ -16,7 +16,6 @@ const data={
 ],
     products: [
         {
-          _id: '1',
           name: 'Yoke Rayon Top',
           category: 'Top',
           image: '/images/p1.jpg',
@@ -28,7 +27,7 @@ const data={
           description: 'high quality Ladies product',
         },
         {
-            _id: '2',
+          
             name: 'Saakaa woman Top',
             category: 'Top',
             image: '/images/p2.jpg',
@@ -40,7 +39,7 @@ const data={
             description: 'high quality Ladies product',
           },
           {
-            _id: '3',
+           
             name: 'Jazzy Ben Top',
             category: 'Top',
             image: '/images/p3.jpg',
@@ -52,7 +51,7 @@ const data={
             description: 'high quality Ladies product',
           },
           {
-            _id: '4',
+           
             name: 'Zaphire Multicolored Top',
             category: 'Top',
             image: '/images/p4.jpg',
@@ -64,7 +63,7 @@ const data={
             description: 'high quality Ladies product',
           },
           {
-            _id: '5',
+           
             name: 'Fabrange regular Top',
             category: 'Top',
             image: '/images/p5.jpg',
@@ -76,7 +75,7 @@ const data={
             description: 'high quality Ladies product',
           },
           {
-            _id: '6',
+            
             name: 'Tunics Ladies Top',
             category: 'Top',
             image: '/images/p6.jpg',
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
+import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
@@ -25,6 +25,7 @@ app.get('/api/products', (req, res) => {
 });
 
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
