76246a49b7942ee7938c989fd01b769c6b823c65
poojaDaithankar <pujadaithankar@gmail.com>
2021-05-13 15:14:26 +0530

    design signin screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './Screens/CartScreen';
 import HomeScreen from './Screens/HomeScreen';
 import productScreen from './Screens/ProductScreen';
+import SigninScreen from './Screens/SigninScreen';
+import { signout } from './actions/userActions';
 
 
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const {cartItems} = cart;
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   
   return (
     <BrowserRouter>
@@ -26,13 +35,29 @@ function App() {
                 <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            <Link to="/signin">Sign In</Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
 </div>
 </header>
 <main>
  <Route path="/cart/:id?" component={CartScreen}></Route>
   <Route path="/product/:id" component={productScreen}></Route>
   <Route path="/" component={HomeScreen} exact></Route>
+  <Route path="/signin" component={SigninScreen}></Route>
   
     </main>
     <footer className="row center">All rights reserved</footer>
